c900a1b4335bdf194d5c1f2e6987d4b15d09312a
Chris Winberry <chris@winberry.net>
2017-04-16 16:09:56 -0400

    Disabled modules install

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -26,44 +26,47 @@ run_if_present() {
 }
 
 yarn_node_modules() {
-  local build_dir=${1:-}
-
-  echo "Installing node modules (yarn.lock)"
-  cd "$build_dir"
-  yarn install --pure-lockfile --ignore-engines 2>&1
+  echo "Not installing node modules"
+  # local build_dir=${1:-}
+  #
+  # echo "Installing node modules (yarn.lock)"
+  # cd "$build_dir"
+  # yarn install --pure-lockfile --ignore-engines 2>&1
 }
 
 npm_node_modules() {
-  local build_dir=${1:-}
-
-  if [ -e $build_dir/package.json ]; then
-    cd $build_dir
-
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing node modules (package.json + shrinkwrap)"
-    else
-      echo "Installing node modules (package.json)"
-    fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
-  else
-    echo "Skipping (no package.json)"
-  fi
+  echo "Not installing node modules"
+  # local build_dir=${1:-}
+  #
+  # if [ -e $build_dir/package.json ]; then
+  #   cd $build_dir
+  #
+  #   if [ -e $build_dir/npm-shrinkwrap.json ]; then
+  #     echo "Installing node modules (package.json + shrinkwrap)"
+  #   else
+  #     echo "Installing node modules (package.json)"
+  #   fi
+  #   npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+  # else
+  #   echo "Skipping (no package.json)"
+  # fi
 }
 
 npm_rebuild() {
-  local build_dir=${1:-}
-
-  if [ -e $build_dir/package.json ]; then
-    cd $build_dir
-    echo "Rebuilding any native modules"
-    npm rebuild 2>&1
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing any new modules (package.json + shrinkwrap)"
-    else
-      echo "Installing any new modules (package.json)"
-    fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
-  else
-    echo "Skipping (no package.json)"
-  fi
+  echo "Not installing node modules"
+  # local build_dir=${1:-}
+  #
+  # if [ -e $build_dir/package.json ]; then
+  #   cd $build_dir
+  #   echo "Rebuilding any native modules"
+  #   npm rebuild 2>&1
+  #   if [ -e $build_dir/npm-shrinkwrap.json ]; then
+  #     echo "Installing any new modules (package.json + shrinkwrap)"
+  #   else
+  #     echo "Installing any new modules (package.json)"
+  #   fi
+  #   npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+  # else
+  #   echo "Skipping (no package.json)"
+  # fi
 }
